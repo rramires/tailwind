3fc60695f3ee134da86ad17564cef33ad880fe13
rramires <ricardo@flexbr.com>
2026-04-11 22:09:36 -0300

    feat: create reusable input components for improved search functionality in sidebar

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,36 @@
+import { type ComponentProps } from 'react'
+
+/* 
+ Component using the React Composition Pattern
+*/
+
+/* A border that will contain the other elements. */
+
+type InputContainerProps = ComponentProps<'div'>
+export function InputContainer(props: InputContainerProps) {
+	return (
+		<div
+			className='mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm'
+			{...props}
+		/>
+	)
+}
+
+/* Icon or image to place before or/and after the control */
+
+type InputAssetProps = ComponentProps<'div'>
+export function InputAsset(props: InputAssetProps) {
+	return <div {...props} />
+}
+
+/* Form element */
+
+type InputControlProps = ComponentProps<'input'>
+export function InputControl(props: InputControlProps) {
+	return (
+		<input
+			className='flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600'
+			{...props}
+		/>
+	)
+}
