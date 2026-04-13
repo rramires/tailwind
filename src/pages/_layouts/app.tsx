9839491e1fc745a269634a2a026467cda628a6f7
rramires <ricardo@flexbr.com>
2026-04-12 22:04:01 -0300

    feat: enhance Home component with new form fields and layout adjustments

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from '@/components/sidebar/sidebar'
 
 export function AppLayout() {
 	return (
-		<div className='grid min-h-screen w-screen grid-cols-[minmax(18rem,20rem)_1fr] antialiased'>
+		<div className='grid-cols-layout grid min-h-screen w-screen antialiased'>
 			<Sidebar />
 			<main className='px-8 pt-8 pb-12'>
 				{/* Content will change here */}
--- src/pages/app/home/home.tsx
+++ src/pages/app/home/home.tsx
@@ -1,5 +1,7 @@
 import { PageTitle } from '@components/title/page-title'
 
+import { InputContainer, InputControl } from '@/components/ui/input'
+
 import { FormActions } from './form-actions'
 import { SettingsTabs } from './tabs'
 
@@ -25,9 +27,134 @@ export function Home() {
 					<FormActions form='settings' />
 				</div>
 
-				<form id='settings' className='mt-6 flex w-full flex-col gap-5'>
-					form
+				<form
+					id='settings'
+					className='mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200'
+				>
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='firstName'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Name
+						</label>
+						<div className='grid grid-cols-2 gap-6'>
+							<InputContainer>
+								<InputControl
+									id='firstName'
+									placeholder='Your First Name'
+								/>
+							</InputContainer>
+							<InputContainer>
+								<InputControl
+									id='lastName'
+									placeholder='Your Last Name'
+								/>
+							</InputContainer>
+						</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='email'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Email
+						</label>
+						<div className=''>
+							<InputContainer>
+								<InputControl
+									id='email'
+									placeholder='email@example.com'
+								/>
+							</InputContainer>
+						</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='email'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Your photo
+							<span className='mt-0.5 block text-sm font-normal text-zinc-500'>
+								This will be displayed on yout profile.
+							</span>
+						</label>
+						<div className=''>{/* TODO: Implement this */}</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='role'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Role
+						</label>
+						<div className=''>
+							<InputContainer>
+								<InputControl id='role' placeholder='ADMIN' />
+							</InputContainer>
+						</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='country'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Country
+						</label>
+						<div className=''>{/* TODO: Implement this */}</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='timezone'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Timezone
+						</label>
+						<div className=''>{/* TODO: Implement this */}</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='bio'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Bio
+							<span className='mt-0.5 block text-sm font-normal text-zinc-500'>
+								Write a short introduction.
+							</span>
+						</label>
+						<div className=''>{/* TODO: Implement this */}</div>
+						<div />
+					</div>
+
+					<div className='grid-cols-form grid gap-3 pb-5'>
+						<label
+							htmlFor='projects'
+							className='text-sm font-medium text-zinc-700'
+						>
+							Portfolio Projects
+							<span className='mt-0.5 block text-sm font-normal text-zinc-500'>
+								Share a few snippets of your work
+							</span>
+						</label>
+						<div className=''>{/* TODO: Implement this */}</div>
+						<div />
+					</div>
 				</form>
+				<div className='flex justify-end pt-5'>
+					<FormActions form='settings' />
+				</div>
 			</div>
 		</>
 	)
